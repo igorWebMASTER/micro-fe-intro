e008d3d39de595002367e09ebe426c7b9fe968a8
igorWebMASTER <igor_1917@hotmail.com>
2022-02-10 21:26:32 -0300

    chore: add error boundary safe component

--- home/src/Header.jsx
+++ home/src/Header.jsx
@@ -82,10 +82,10 @@ export default function Header() {
   return (
     <Popover className="relative bg-white">
       <div className="max-w-100 mx-auto ">
-        <div className="flex justify-between items-center border-b-2 border-gray-100 py-6 md:justify-start md:space-x-10 xl:px-6">
+        <div className="flex justify-between items-center border-b-2 border-gray-100 py-6 md:justify-start md:space-x-10 px-6">
           <div className="flex justify-start lg:w-0 lg:flex-1">
             <a href="#">
-              <span className="sr-only">Workflow</span>
+              <span className="sr-only">SDSDs</span>
               <img
                 className="h-8 w-auto sm:h-10"
                 src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
