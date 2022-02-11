2522d84a3cef449e3c2943f58a9356e05511a0c3
igor.sobral <igor.sobral@pagtel.com.br>
2022-02-10 21:25:17 -0300

    chore: add error boundary safe component

--- pdp/src/App.jsx
+++ pdp/src/App.jsx
@@ -1,16 +1,27 @@
-import React from "react";
+import React, { Suspense } from "react";
 import ReactDOM from "react-dom";
 
 import Footer from "home/Footer";
 import Header from "home/Header";
+import SafeComponent from "./SafeComponent"
 
 import "./index.scss";
 
-const App = () => (
-  <div className="mt-10 text-3xl mx-auto max-w-6xl">
-    <Header />
-    <div className="my-10">PDP page content</div>
-    <Footer />
-  </div>
-);
+const App = () => {
+
+  return (
+    <div className="mt-10 text-3xl mx-auto max-w-6xl">
+      <SafeComponent>
+      <Header/>
+      </SafeComponent>
+      <button className="text-3xl p-5"
+        onClick={() => setShowHeader(!showHeader)}
+      >
+
+      </button>
+      <div className="my-10">PDP page content</div>
+      <Footer />
+    </div>
+  )
+};
 ReactDOM.render(<App />, document.getElementById("app"));
